bae8fb1c3a37dcb7d70bcd128b717afb59d63579
thiagosemann <thiago_semann@hotmail.com>
2019-07-03 13:32:49 -0300

    Projeto Iniciado

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyrigth 2019  
+            <a href='http://coder.com.br' target='_blank'>Cod3r</a>. 
+        </strong>
+    </footer>
+)
---
+++ frontend/src/dashboard/dashboard.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import {getSummary} from './dashboardActions'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
+
+
+class Dashboard extends Component {
+    componentWillMount (){
+        this.props.getSummary()
+    }
+
+    render(){
+        const {credit, debt} = this.props.summary 
+        return(
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            text='Total de Cŕeditos' value={`R$ ${credit}`} />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            text='Total de Débitos' value={`R$ ${debt}`}/>
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            text='Total de Valor Consolidado' value={`R$ ${credit- debt}`} />
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps,mapDispatchToProps)(Dashboard)
+
